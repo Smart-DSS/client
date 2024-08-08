c627be5bcba62c2d35de1066b65e9464b055c237
Joshua <joshu2@MacBook-Air.local>
2024-08-08 22:15:27 +0530

    minor changes

--- app/(routes)/dashboard/_components/CrowdDetails.jsx
+++ app/(routes)/dashboard/_components/CrowdDetails.jsx
@@ -5,41 +5,44 @@ import { doc, getDoc, getFirestore } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 
 const CrowdDetails = () => {
-  //   const db = getFirestore(app);
-  //   const [rainStatus, setRainStatus] = useState();
-  //   const [waterLevel, setWaterLevel] = useState();
-  //   const getFloodData = async () => {
-  //     const docRef = doc(db, "Flood-data", "waterlevel-1");
-  //     try {
-  //       const docSnap = await getDoc(docRef);
-  //       if (docSnap.exists()) {
-  //         setRainStatus(docSnap.data()?.rainStatus);
-  //         setWaterLevel(docSnap.data()?.waterLevel);
-  //       } else {
-  //         console.log("No such stage!");
-  //       }
-  //     } catch (error) {
-  //       console.log("No such stage!");
-  //     }
-  //   };
-
-  //   useEffect(() => {
-  //     getFloodData();
-  //   }, []);
-
-  const getColor = (value) => {
-    if (value > 20) {
-      return "text-red-600";
-    } else if (value > 10) {
-      return "text-orange-600";
-    } else {
-      return "text-green-600";
+  const db = getFirestore(app);
+  const [camera_id, setCamera_id] = useState();
+  const [person_count, setPerson_count] = useState();
+  const [timestamp, setTimestamp] = useState();
+  const getCrowdData = async () => {
+    const docRef = doc(db, "real_time_data", "crowd");
+    try {
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setCamera_id(docSnap.data()?.camera_id);
+        setPerson_count(docSnap.data()?.person_count);
+        setTimestamp(docSnap.data()?.timestamp);
+      } else {
+        console.log("No such stage!");
+      }
+    } catch (error) {
+      console.log("No such stage!");
     }
+  };
+
+  useEffect(() => {
+    getCrowdData();
+  }, []);
+
+  const getColor = () => {
+    return "text-blue-900";
+    // if (value > 20) {
+    //   return "text-red-600";
+    // } else if (value > 10) {
+    //   return "text-orange-600";
+    // } else {
+    //   return "text-green-600";
+    // }
   };
 
   return (
     <div className="flex justify-center p-[1%]">
-      <div className="w-full h-24 bg-[#f1f1f1] rounded-[15px] flex justify-between p-[5%] hover:scale-105 hover:cursor-pointer">
+      <div className="w-full h-28 bg-[#f1f1f1] rounded-[15px] flex justify-between p-[5%] hover:scale-105 hover:cursor-pointer">
         <div className="flex flex-col justify-centern w-[50%]">
           <div className="flex flex-col justify-center">
             <div className="text-black text-xs font-semibold font-['Radio Canada'] tracking-wide">
@@ -47,25 +50,30 @@ const CrowdDetails = () => {
             </div>
             <div>
               <span className="text-black text-xs font-semibold font-['Radio Canada'] tracking-wide">
-                Clogging:{" "}
+                camera_id:{" "}
               </span>
               <span
-                className={`text-xs font-semibold font-['Radio Canada'] tracking-wide ${getColor(
-                  12
-                )}`}
+                className={`text-xs font-semibold font-['Radio Canada'] tracking-wide ${getColor()}`}
               >
-                12
+                {camera_id}
                 <br />
               </span>
               <span className="text-black text-xs font-semibold font-['Radio Canada'] tracking-wide">
-                Bottleneck:{" "}
+                Time:{" "}
               </span>
               <span
-                className={`text-xs font-semibold font-['Radio Canada'] tracking-wide ${getColor(
-                  7
-                )}`}
+                className={`text-xs font-semibold font-['Radio Canada'] tracking-wide ${getColor()}`}
               >
-                7
+                {timestamp && typeof timestamp === 'string' ? timestamp.split(' ')[1] : ''}
+                <br />
+              </span>
+              <span className="text-black text-xs font-semibold font-['Radio Canada'] tracking-wide">
+                Date:{" "}
+              </span>
+              <span
+                className={`text-xs font-semibold font-['Radio Canada'] tracking-wide ${getColor()}`}
+              >
+                {timestamp && typeof timestamp === 'string' ? timestamp.split(' ')[0] : ''}
                 <br />
               </span>
             </div>
@@ -73,10 +81,10 @@ const CrowdDetails = () => {
         </div>
         <div className="flex flex-col justify-center">
           <div className="text-black text-xs font-semibold font-['Radio Canada'] tracking-wide">
-            Diverging Flow:
+            Person count:
           </div>
-          <div className="text-black text-2xl font-semibold font-['Radio Canada'] tracking-widest flex justify-center">
-            7
+          <div className="text-black text-5xl font-semibold font-['Radio Canada'] tracking-widest flex justify-center">
+            {person_count}
           </div>
         </div>
       </div>
--- app/(routes)/dashboard/_components/LocationCards.jsx
+++ app/(routes)/dashboard/_components/LocationCards.jsx
@@ -3,13 +3,17 @@ import React from "react";
 const LocationCards = () => {
   return (
     <div className="w-full h-full max-h-[350px] md:max-h-[350px] flex flex-col m-2 p-2 rounded-lg bg-white shadow-[4px_8px_15px_#00000040]">
-      <button className="w-[95%] m-2 p-3 bg-gray-200 text-gray-800 border-none rounded-md cursor-pointer transition-all hover:bg-gray-300 transform hover:scale-95">
+      {/* <button className="w-[95%] m-2 p-3 bg-[#f1f1f1] text-gray-800 border-none rounded-md cursor-pointer transition-all hover:bg-gray-300 transform hover:scale-95">
         Sort by Alert Percentage
-      </button>
+      </button> */}
+      <div className="w-full flex justify-center items-center text-black text-lg font-['Radio Canada'] tracking-wide">
+        <div>Sort by Alert Percentage</div>
+      </div>
+      <div className="w-full h-[0px] border border-black m-1"></div>
       <div className="flex-1 overflow-y-auto pr-1">
-        <div className="bg-gray-200 my-1.5 p-4 rounded-lg transition-transform transform hover:scale-95 cursor-pointer">
+        <div className="bg-[#f1f1f1] my-1.5 p-4 rounded-lg transition-transform transform hover:scale-95 cursor-pointer">
           <div className="flex justify-between items-center">
-            <h3>Pambla</h3>
+            <h3>S.M. Street</h3>
             <div className="relative">
               <div className="absolute top-[-1.55rem] right-[-0.15rem] bg-red-600 text-white p-1 rounded-full text-xs font-medium">
                 ALERT
@@ -21,9 +25,9 @@ const LocationCards = () => {
           </div>
         </div>
 
-        <div className="bg-gray-200 my-1.5 p-4 rounded-lg transition-transform transform hover:scale-95 cursor-pointer">
+        <div className="bg-[#f1f1f1] my-1.5 p-4 rounded-lg transition-transform transform hover:scale-95 cursor-pointer">
           <div className="flex justify-between items-center">
-            <h3>Kallarkutty</h3>
+            <h3>Kozhikode Beach</h3>
             <div className="relative">
               <div className="absolute top-[-1.55rem] right-[-0.15rem] bg-red-600 text-white p-1 rounded-full text-xs font-medium">
                 ALERT
@@ -35,9 +39,9 @@ const LocationCards = () => {
           </div>
         </div>
 
-        <div className="bg-gray-200 my-1.5 p-4 rounded-lg transition-transform transform hover:scale-95 cursor-pointer">
+        <div className="bg-[#f1f1f1] my-1.5 p-4 rounded-lg transition-transform transform hover:scale-95 cursor-pointer">
           <div className="flex justify-between items-center">
-            <h3>Sholayar</h3>
+            <h3>Krishna Menon Museum</h3>
             <div className="relative">
               <div className="bg-gray-100 text-gray-800 p-1 rounded-md text-sm font-medium">
                 36.00%
@@ -46,9 +50,9 @@ const LocationCards = () => {
           </div>
         </div>
 
-        <div className="bg-gray-200 my-1.5 p-4 rounded-lg transition-transform transform hover:scale-95 cursor-pointer">
+        <div className="bg-[#f1f1f1] my-1.5 p-4 rounded-lg transition-transform transform hover:scale-95 cursor-pointer">
           <div className="flex justify-between items-center">
-            <h3>Kallar</h3>
+            <h3>Mananchira Square</h3>
             <div className="relative">
               <div className="bg-gray-100 text-gray-800 p-1 rounded-md text-sm font-medium">
                 33.04%
@@ -57,9 +61,9 @@ const LocationCards = () => {
           </div>
         </div>
 
-        <div className="bg-gray-200 my-1.5 p-4 rounded-lg transition-transform transform hover:scale-95 cursor-pointer">
+        <div className="bg-[#f1f1f1] my-1.5 p-4 rounded-lg transition-transform transform hover:scale-95 cursor-pointer">
           <div className="flex justify-between items-center">
-            <h3>Pamba</h3>
+            <h3>Kozhikode Railway Station</h3>
             <div className="relative">
               <div className="bg-gray-100 text-gray-800 p-1 rounded-md text-sm font-medium">
                 25.49%
@@ -68,9 +72,9 @@ const LocationCards = () => {
           </div>
         </div>
 
-        <div className="bg-gray-200 my-1.5 p-4 rounded-lg transition-transform transform hover:scale-95 cursor-pointer">
+        <div className="bg-[#f1f1f1] my-1.5 p-4 rounded-lg transition-transform transform hover:scale-95 cursor-pointer">
           <div className="flex justify-between items-center">
-            <h3>Kundala</h3>
+            <h3>Tali Temple</h3>
             <div className="relative">
               <div className="bg-gray-100 text-gray-800 p-1 rounded-md text-sm font-medium">
                 22.72%
@@ -79,39 +83,9 @@ const LocationCards = () => {
           </div>
         </div>
 
-        <div className="bg-gray-200 my-1.5 p-4 rounded-lg transition-transform transform hover:scale-95 cursor-pointer">
+        <div className="bg-[#f1f1f1] my-1.5 p-4 rounded-lg transition-transform transform hover:scale-95 cursor-pointer">
           <div className="flex justify-between items-center">
-            <h3>Anayirankal</h3>
-            <div className="relative">
-              <div className="bg-gray-100 text-gray-800 p-1 rounded-md text-sm font-medium">
-                18.61%
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="bg-gray-200 my-1.5 p-4 rounded-lg transition-transform transform hover:scale-95 cursor-pointer">
-          <div className="flex justify-between items-center">
-            <h3>Anayirankal</h3>
-            <div className="relative">
-              <div className="bg-gray-100 text-gray-800 p-1 rounded-md text-sm font-medium">
-                18.61%
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="bg-gray-200 my-1.5 p-4 rounded-lg transition-transform transform hover:scale-95 cursor-pointer">
-          <div className="flex justify-between items-center">
-            <h3>Anayirankal</h3>
-            <div className="relative">
-              <div className="bg-gray-100 text-gray-800 p-1 rounded-md text-sm font-medium">
-                18.61%
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="bg-gray-200 my-1.5 p-4 rounded-lg transition-transform transform hover:scale-95 cursor-pointer">
-          <div className="flex justify-between items-center">
-            <h3>Anayirankal</h3>
+            <h3>Muthalakkulam Mosque</h3>
             <div className="relative">
               <div className="bg-gray-100 text-gray-800 p-1 rounded-md text-sm font-medium">
                 18.61%
