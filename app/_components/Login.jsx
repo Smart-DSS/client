c68e756c9e17e166af9dfc255f7aae932218b083
Joshua <joshu2@MacBook-Air.local>
2024-08-09 11:54:32 +0530

    minor changes in login

--- app/_components/Login.jsx
+++ app/_components/Login.jsx
@@ -8,18 +8,16 @@ import React from "react";
 const Login = () => {
   return (
     <div
+      // className="h-screen pt-[5%] bg-[#4255e0] bg-opacity-90"
       className="h-screen pt-[5%]"
       style={{
         backgroundImage: "url('/background.svg')",
       }}
     >
-      <div className="bg-white/90 rounded-2xl w-[90%] h-[90%] mx-auto p-[5%] ">
+      <div className="bg-white/90 rounded-2xl w-[90%] h-[90%] mx-auto p-[5%] flex flex-col justify-center">
         {/* left */}
-        <div className="flex flex-col justify-center items-center ">
+        <div className="flex flex-col justify-evenly h-full">
           <div className="text-center items-center mt-[2%]">
-            <p className="font-bold text-[18px] md:text-[24px] w-[100%] mx-auto">
-              SMART DSS FOR CROWD MANAGEMENT
-            </p>
             <Image
               src={"/GOI.svg"}
               width={500}
@@ -28,10 +26,21 @@ const Login = () => {
               className="p-5 flex justify-center items-center mx-auto "
               // className="w-[50px] md:w-[60px] p-1"
             />
-            <h1 className="text-lg md:text-xl mt-5 text-slate-500 px-4">
-              Enhance Safety and Efficiency using AI, Real-Time Data Analytics,
-              and Predictive Algorithms.
-            </h1>
+            {/* <p className="font-bold text-[18px] md:text-[24px] w-[100%] mx-auto">
+              NIRNAI
+            </p> */}
+            <p className="[font-family:'Sofia_Sans-Thin',Helvetica] font-normal text-[24px] w-[100%] mx-auto">
+              <span className="font-extralight">NIRN</span>
+              <span className="[font-family:'Sofia_Sans-Black',Helvetica] font-black">
+                AI
+              </span>
+            </p>
+            <div className="w-full items-center flex justify-center px-[5%] md:px-[20%]">
+              <h1 className="text-lg md:text-xl mt-5 text-slate-500 px-4">
+                AI-based DSS framework for Disaster preparedness for flood
+                management and crowd monitoring.
+              </h1>
+            </div>
             <div className="flex gap-4 flex-col mt-5">
               <div className="flex justify-center gap-2 md:gap-8 px-2">
                 <LoginLink>
