360292084e646ec85a02e3ec00865254b8387df3
Joshua <joshu2@MacBook-Air.local>
2024-08-24 16:41:20 +0530

    minor changes

--- app/api/route1/route.js
+++
@@ -1,8 +0,0 @@
-import prisma from '@/lib/prisma';
-
-export async function GET(req) {
-  const users = await prisma.user.findMany();
-  return new Response(JSON.stringify(users), {
-    headers: { 'Content-Type': 'application/json' },
-  });
-}
