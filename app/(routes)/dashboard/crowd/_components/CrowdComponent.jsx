0fdb55be41b8b786da4a5005e00de12638b6af96
Joshua <joshu2@MacBook-Air-2.local>
2024-10-23 14:09:24 +0530

    Internship completion

--- app/(routes)/dashboard/crowd/_components/CrowdComponent.jsx
+++ app/(routes)/dashboard/crowd/_components/CrowdComponent.jsx
@@ -5,41 +5,41 @@ import CrowdDetails from "./CrowdDetails";
 const CrowdComponent = () => {
   const [crowdData, setCrowdData] = useState([]);
 
-  // const getCrowdData = async () => {
-  //   try {
-  //     const data = await fetch('/api/crowd-data');
-  //     const result = await data.json();
+  const getCrowdData = async () => {
+    try {
+      const data = await fetch("/api/crowd-data");
+      const result = await data.json();
 
-  //     // Group the data by camera_id and keep the entry with the latest id for each
-  //     const latestCrowdData = Object.values(
-  //       result.reduce((acc, current) => {
-  //         const existing = acc[current.camera_id];
+      // Group the data by camera_id and keep the entry with the latest id for each
+      const latestCrowdData = Object.values(
+        result.reduce((acc, current) => {
+          const existing = acc[current.camera_id];
 
-  //         // Compare the id to determine the latest entry
-  //         if (!existing || existing.id < current.id) {
-  //           acc[current.camera_id] = current;
-  //         }
-  //         return acc;
-  //       }, {})
-  //     );
-  //     console.log(latestCrowdData);
-  //     // Set the formatted data to the state
-  //     setCrowdData(latestCrowdData);
-  //   } catch (error) {
-  //     console.error("Failed to fetch crowd data:", error);
-  //   }
-  // };
+          // Compare the id to determine the latest entry
+          if (!existing || existing.id < current.id) {
+            acc[current.camera_id] = current;
+          }
+          return acc;
+        }, {})
+      );
+      console.log(latestCrowdData);
+      // Set the formatted data to the state
+      setCrowdData(latestCrowdData);
+    } catch (error) {
+      console.error("Failed to fetch crowd data:", error);
+    }
+  };
 
-  // useEffect(() => {
-  //   // Fetch data when component mounts
-  //   getCrowdData();
+  useEffect(() => {
+    // Fetch data when component mounts
+    getCrowdData();
 
-  //   // Polling every 5 seconds
-  //   const intervalId = setInterval(getCrowdData, 1000);
+    // Polling every 5 seconds
+    const intervalId = setInterval(getCrowdData, 1000);
 
-  //   // Cleanup on component unmount
-  //   return () => clearInterval(intervalId);
-  // }, []);
+    // Cleanup on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <div className="w-full h-full max-h-[400px] md:max-h-[640px] flex flex-col m-2 p-2 rounded-lg bg-white shadow-[4px_8px_15px_#00000040]">
@@ -48,15 +48,15 @@ const CrowdComponent = () => {
       </div>
       <div className="w-full h-[0px] border border-black m-1"></div>
       <div className="w-full h-full p-[2%] overflow-y-auto">
-        {/* {crowdData.map((data) => (
+        {crowdData.map((data) => (
           <CrowdDetails
             key={data.camera_id}
             camera_id={data.camera_id}
             person_count={data.count_camera}
-            timestamp={dayjs(data.timestamp).format('YYYY-MM-DD HH:mm:ss')}
+            timestamp={dayjs(data.timestamp).format("YYYY-MM-DD HH:mm:ss")}
           />
-        ))} */}
-        <CrowdDetails
+        ))}
+        {/* <CrowdDetails
           key='1'
           camera_id='1'
           person_count='2'
@@ -73,8 +73,7 @@ const CrowdComponent = () => {
           camera_id='3'
           person_count='4'
           timestamp="2024-09-20 14:51:05"
-        />
-        
+        /> */}
       </div>
     </div>
   );
